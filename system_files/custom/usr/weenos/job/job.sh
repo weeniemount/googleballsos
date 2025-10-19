ffbb3569f6a139bc065154f06783829edae9e6c9
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-10-19 19:27:59 +0200

    Update job.sh

--- system_files/custom/usr/weenos/job/job.sh
+++ system_files/custom/usr/weenos/job/job.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# job jumpscare if someone trys to run nvim, vim, neovim, or vi
+# job jumpscare if someone tries to run nvim, vim, neovim, or vi
 IMAGE="/usr/weenos/job/job.png"
 SFX="/usr/weenos/job/job.mp3"
 
-feh --fullscreen --zoom fill "$IMAGE" &
-ffplay -nodisp -autoexit "$SFX"
+(
+	feh --fullscreen --zoom fill "$IMAGE" >/dev/null 2>&1 &
 
-sleep 2
+	ffplay -nodisp -autoexit "$SFX" >/dev/null 2>&1
 
-# kill the job application
-pkill feh
+	sleep 2
+
+	# kill the job application
+	pkill feh >/dev/null 2>&1
+) >/dev/null 2>&1 &
+disown
