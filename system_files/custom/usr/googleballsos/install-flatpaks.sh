fd5a18a24eb35212aa0f1210c3a4464b8f9f4aaa
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-10-19 17:03:21 +0200

    googleballsos-install-flatpaks

---
+++ system_files/custom/usr/googleballsos/install-flatpaks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# googleballsos-install-flatpaks
+
+FLATPAK_LIST="/usr/googleballsos/flatpakList.txt"
+
+if [ ! -f "$FLATPAK_LIST" ]; then
+	echo "weeniemount is bad at coding"
+	exit 1
+fi
+
+echo "installing the optional google balls os flatpaks"
+while IFS= read -r pkg; do
+	[ -z "$pkg" ] && continue
+	[[ "$pkg" =~ ^# ]] && continue
+	
+	echo "installing $pkg..."
+	flatpak install -y "$pkg"
+done < "$FLATPAK_LIST"
+
+echo "done! enjoy your optimal google balls"
