aa337799e3431d474943d23d439d299a8738164f
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-10-20 10:58:50 +0200

    Update install-flatpaks.sh

--- system_files/custom/usr/weenos/install-flatpaks.sh
+++ system_files/custom/usr/weenos/install-flatpaks.sh
@@ -14,16 +14,17 @@ while IFS= read -r pkg; do
 	[ -z "$pkg" ] && continue
 	[[ "$pkg" =~ ^# ]] && continue
 
-	echo "installing $pkg"
+	printf "installing %s" "$pkg"
 	flatpak install -y "$pkg" >/dev/null 2>&1 &
 	pid=$!
 
 	i=0
 	while kill -0 $pid 2>/dev/null; do
-		printf "\r${spinner:i++%${#spinner}:1}"
+		printf "\r${spinner:i++%${#spinner}:1} installing %s" "$pkg"
 		sleep 0.1
 	done
 	wait $pid
+	printf "\n"
 done < "$FLATPAK_LIST"
 
 echo "done! enjoy your optimal weenOS experience"
