ad6cf2e9a81d1dbdbbe80ba53850c578d29a13e0
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-10-25 17:03:07 +0200

    okay maybe this one

--- build_files/build.sh
+++ build_files/build.sh
@@ -43,5 +43,7 @@ git clone https://github.com/wilversings/maxwell /usr/share/plasma/plasmoids/max
 sudo plymouth-set-default-theme logo-slider
 
 # and then forcefully rebuild initrd!!!!... maybe this one will work
-QUALIFIED_KERNEL="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' "kernel${KERNEL_SUFFIX:+-${KERNEL_SUFFIX}}")"
+QUALIFIED_KERNEL="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' "kernel")"
 /usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+
+chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
